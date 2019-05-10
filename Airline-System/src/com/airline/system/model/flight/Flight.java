521dc6631c4311e64e5c0d658e05279303f31292
MichalZmu <michal.zmuda.mail@gmail.com>
2019-05-10 07:56:01 +0200

    Some changes in flight class

--- Airline-System/src/com/airline/system/model/flight/Flight.java
+++ Airline-System/src/com/airline/system/model/flight/Flight.java
@@ -4,12 +4,17 @@ import java.util.Map;
 
 public class Flight {
 	
-	private String flightNumber;
+	private final String flightNumber;
 	private Plane planeNumber;
+	private final String destination;
+	private final String origin;
+	private TreeMap<String, Seat> = new TreeMap()<>;
 	
-	public Flight(String flightNumber, Plane planeNumber) {
+	public Flight(String flightNumber, Plane planeNumber, String destination, String origin) {
 		this.flightNumber = flightNumber;
 		this.planeNumber = planeNumber;
+		this.destination = destination;
+		this.origin = origin;
 	}
 
 }
